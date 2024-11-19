c3c5f705335533d8eb47f542728a38a75db63e42
Natalie <nenman@uw.edu>
2024-11-19 10:43:39 -0800

    ignoring keys.py

--- app.py
+++ app.py
@@ -8,4 +8,11 @@ def index():
             {"name": "oranges", "quantity": 2},
             {"name": "strawberries", "quantity": 6}
     ]
+    for fruit in fruits:
+        if not fruit["name"].startswith("o"):
+            fruits.remove(fruit)
+        elif fruit["quantity"] >= 3:
+            fruits.remove(fruit)
+
+
     return render_template("index.html", fruits=fruits)
