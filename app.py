34a7973e47915dba1f0d49dc74a9bcf850c42f73
Natalie <nenman@uw.edu>
2024-11-19 11:11:20 -0800

    keys last step?

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask, render_template
+import keys
 
 app = Flask(__name__)
 @app.route("/")
 def index():
+
     fruits = [
         {"name": "apples", "quantity": 3},
             {"name": "oranges", "quantity": 2},
@@ -15,4 +17,4 @@ def index():
             fruits.remove(fruit)
 
 
-    return render_template("index.html", fruits=fruits)
+    return render_template("index.html", fruits=fruits, key_1=keys.MY_SECRET_API_KEY_1, key_2= keys.MY_SECRET_API_KEY_2)
